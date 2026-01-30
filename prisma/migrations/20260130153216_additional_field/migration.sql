bc60c339cd76fd2a11f3ead115d66c669fc826af
unknown <abuhasanxt@gmail.com>
2026-01-30 21:43:52 +0600

    added user additional fields

---
+++ prisma/migrations/20260130153216_additional_field/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "student" ADD COLUMN     "phone" TEXT,
+ADD COLUMN     "role" TEXT DEFAULT 'STUDENT';
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,6 +6,20 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql", // or "mysql", "postgresql", ...etc
   }),
+  trustedOrigins:[process.env.APP_URL!],
+  user:{
+additionalFields:{
+    role:{
+        type:"string",
+        defaultValue:"STUDENT",
+        required:false
+    },
+    phone:{
+        type:"string",
+        required:false
+    }
+}
+  },
   emailAndPassword: {
     enabled: true,
   },
