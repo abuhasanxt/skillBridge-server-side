53d804f24d273d6e2d3580d7290a719d94d29612
unknown <abuhasanxt@gmail.com>
2026-01-31 20:10:46 +0600

    social authentication with google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -157,4 +157,15 @@ export const auth = betterAuth({
       }
     },
   },
+
+  
+ socialProviders: {
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID as string, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+        }, 
+    },
+
+
+
 });
